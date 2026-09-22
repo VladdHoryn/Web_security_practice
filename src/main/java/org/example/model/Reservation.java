d582fcb34994b11196080014d2697758770a120c
VladdHoryn <uzerok88@gmail.com>
2026-09-22 12:55:26 +0300

    feat: create base for Reservation class

---
+++ src/main/java/org/example/model/Reservation.java
@@ -0,0 +1,38 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+/*
+  @author   VladHoryn
+  @project   Code
+  @class  Reservation
+  @version  1.0.0 
+  @since 22.09.2026 - 12.52
+*/
+@Entity
+@Table(name = "reservations")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(name = "reservation_date", nullable = false)
+    private LocalDate reservationDate;
+}
