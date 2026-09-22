d8434e8d986eecce097e847515e21101f8ec3c07
VladdHoryn <uzerok88@gmail.com>
2026-09-22 13:20:59 +0300

    feat: create migrations for seeding tables: books, users, reservations

---
+++ src/main/resources/db/migration/V5__seed_users_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email)
+VALUES ('Олександр Петренко', 'oleksandr.petренко@example.com'),
+       ('Марія Коваленко', 'm.kovalenko@example.com'),
+       ('Іван Сидоренко', 'ivan.sydorenko@example.com'),
+       ('Олена Бойко', 'olena.boyko@example.com'),
+       ('Дмитро Шевчук', 'dima.shevchuk@example.com'),
+       ('Анна Мельник', 'anna.melnyk@example.com'),
+       ('Тарас Григорчук', 'taras.grygorchuk@example.com');
